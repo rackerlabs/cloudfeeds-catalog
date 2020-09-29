47b08ece3bbaa0dd7d9fc78390065cec0822a329
shub6691 <shubham.pandey@rackspace.com>
2020-09-29 16:43:29 +0530

    Added comments in the files

--- src/main/java/SpringBootMainApplication.java
+++ src/main/java/SpringBootMainApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * This is the main Spring Boot application class.
+ */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @ComponentScan(basePackages="com.rackspace.feeds.feedscatalog")
 public class SpringBootMainApplication {
--- src/main/java/com/rackspace/feeds/feedscatalog/controller/WebController.java
+++ src/main/java/com/rackspace/feeds/feedscatalog/controller/WebController.java
@@ -1,12 +1,15 @@
 package com.rackspace.feeds.feedscatalog.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+This is the main controller class having two methods taking two parameters catalog
+ abd tenantId
+ */
 @RestController
 public class WebController {
 
